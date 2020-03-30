4283daddc4b701acf7e865fd9b4b1265c997b565
Max Bonus <mguzenko@outlook.com>
2020-03-30 03:10:43 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+#You'll be teaching Dog about their names through two methods, #name, and #name= that read and write to a corresponding instance variable @name
+
+class Dog
+
+  def name
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+end
